731114d66237b467d84364b9c7e358ebaa97c45b
Vedang55 <Vedang55.vd@gmail.com>
2020-02-21 15:09:34 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './style.css';
-import Main from './screens/Main'
 import Events from './screens/Events/Events'
 
 function App() {
