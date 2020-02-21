4ede4a01e49ba73336a9bd41e226591784afb3dd
Vedang55 <Vedang55.vd@gmail.com>
2020-02-21 13:44:41 +0530

    update modal

--- src/components/EventTab/UpdateModal.js
+++ src/components/EventTab/UpdateModal.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Modal, Button
 } from 'react-bootstrap'
@@ -16,13 +16,17 @@ const UpdateModel = (props) => {
     const [selectValue, setSelectValue] = useState(props.data.type.toUpperCase());
     const [sending, setSending] = useState(false);
 
+    useEffect(() => {
+        setRulesTextarea(props.data.rules);
+        setEventNameInput(props.data.name);
+        setSelectValue(props.data.type.toUpperCase());
+    }, [props.data]);
 
 
-    
     const handleSubmit = (event) => {
         event.preventDefault();
         setSending(true);
-        var updateEventRef = db.ref('events/'+props.data.key);
+        var updateEventRef = db.ref('events/' + props.data.key);
         updateEventRef.set({
             name: eventNameInput,
             type: selectValue,
@@ -52,59 +56,60 @@ const UpdateModel = (props) => {
 
 
     return (
-        <React.Fragment>
-            <Modal show={props.show} onHide={props.handleClose} animation={false}>
-            <form onSubmit={handleSubmit}>
-                <div className="form-row">
-                    <div className="form-group col-md-8">
-                        <label for="inputEmail4">EVENT NAME</label>
-                        <input type="text"
-                            className="form-control"
-                            // id="inputEmail4"
-                            placeholder="eg. Antakshari"
-                            onChange={eventNameInputChange}
-                            value={eventNameInput}
-                            required
-                        />
+
+        <div style={{padding:'40px'}}>
+            <Modal show={props.show} onHide={props.handleClose} animation={true}>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-row">
+                        <div className="form-group col-md-8">
+                            <label for="inputEmail4">EVENT NAME</label>
+                            <input type="text"
+                                className="form-control"
+                                // id="inputEmail4"
+                                placeholder="eg. Antakshari"
+                                onChange={eventNameInputChange}
+                                value={eventNameInput}
+                                required
+                            />
+                        </div>
+                        <div className="form-group col-md-4">
+                            <label for="inputState">CATEGORY</label>
+                            <select id="inputState" className="form-control" onChange={selectChange} value={selectValue}>
+                                <option>ACE</option>
+                                <option>KING</option>
+                                <option>QUEEN</option>
+                                <option>JACK</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className="form-group col-md-4">
-                        <label for="inputState">CATEGORY</label>
-                        <select id="inputState" className="form-control" onChange={selectChange} value={selectValue}>
-                            <option>ACE</option>
-                            <option>KING</option>
-                            <option>QUEEN</option>
-                            <option>JACK</option>
-                        </select>
+                    <div className="form-group">
+                        <label for="inputAddress">RULES</label>
+                        <textarea className="form-control"
+                            id="exampleFormControlTextarea1" rows="10"
+                            placeholder="Type Each rule on separate line"
+                            value={rulesTextarea}
+                            onChange={rulesTextareaChange}
+                            required>
+                        </textarea>
                     </div>
-                </div>
-                <div className="form-group">
-                    <label for="inputAddress">RULES</label>
-                    <textarea className="form-control"
-                        id="exampleFormControlTextarea1" rows="10"
-                        placeholder="Type Each rule on separate line"
-                        value={rulesTextarea}
-                        onChange={rulesTextareaChange}
-                        required>
-                    </textarea>
-                </div>
 
 
-            </form>
+                </form>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={props.handleClose}>
                         Close
                 </Button>
-                {sending ? (<ClipLoader
-                    size={30}
-                    color={"#123abc"}
-                    loading={sending}
-                />)
-                    : (<button type="submit" onClick={handleSubmit} className="btn btn-primary">Save Changes</button>)}
+                    {sending ? (<ClipLoader
+                        size={30}
+                        color={"#123abc"}
+                        loading={sending}
+                    />)
+                        : (<button type="submit" onClick={handleSubmit} className="btn btn-primary">Save Changes</button>)}
                 </Modal.Footer>
             </Modal>
 
+        </div>
 
-        </React.Fragment>
     );
 }
 
